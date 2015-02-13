255b2e2b31ff7871ab09be24f9bd770792a6a413
Denis Bolshakov <denis.bolshakov@megafon.ru>
2015-02-13 10:39:44 +0300

    Test refactoring to do it more readable, adding a sample file

--- project-1/src/test/java/ru/dbolshak/hadoop/MaxTemperatureTest.java
+++ project-1/src/test/java/ru/dbolshak/hadoop/MaxTemperatureTest.java
@@ -13,16 +13,23 @@ import java.io.IOException;
 import java.util.Arrays;
 
 public class MaxTemperatureTest {
-    private static final String A_YEAR_FOR_TEST = String.valueOf(1950);
-    private static final int LOW_TEMPERATURE = -5;
-    private static final int HIGH_TEMPERATURE = 10;
+    private static final int    A_YEAR_FOR_TEST               = 1950;
+    private static final int    LOW_AIR_TEMPERATURE           = -11;
+    private static final int    HIGH_AIR_TEMPERATURE          = 10;
+    private static final int    AIR_TEMPERATURE_CODE_QUALITY  = 1;
 
     private static Text createYearAsText() {
-        return new Text(A_YEAR_FOR_TEST);
+        return new Text(String.valueOf(A_YEAR_FOR_TEST));
     }
 
     private static String readLineFromFile() {
-        return "0043011990999991950051518004+68750+023550FM-12+038299999V0203201N00261220001CN9999999N9-00111+99999999999";
+        return buildLineWithKnownValues();
+    }
+
+    private static String buildLineWithKnownValues() {
+        return String.format(
+                "004301199099999%d051518004+68750+023550FM-12+038299999V0203201N00261220001CN9999999N9%05d%d+99999999999",
+                A_YEAR_FOR_TEST, LOW_AIR_TEMPERATURE, AIR_TEMPERATURE_CODE_QUALITY);
     }
 
     @Test
@@ -32,7 +39,7 @@ public class MaxTemperatureTest {
         new MapDriver<LongWritable, Text, Text, IntWritable>()
                 .withMapper(new MaxTemperatureMapper())
                 .withInput(new LongWritable(0), value)
-                .withOutput(createYearAsText(), new IntWritable(-11))
+                .withOutput(createYearAsText(), new IntWritable(LOW_AIR_TEMPERATURE))
                 .runTest();
     }
 
@@ -40,8 +47,8 @@ public class MaxTemperatureTest {
     public void returnsMaximumIntegerInValues() throws IOException, InterruptedException {
         new ReduceDriver<Text, IntWritable, Text, IntWritable>()
                 .withReducer(new MaxTemperatureReducer())
-                .withInput(createYearAsText(), Arrays.asList(new IntWritable(HIGH_TEMPERATURE), new IntWritable(LOW_TEMPERATURE)))
-                .withOutput(createYearAsText(), new IntWritable(HIGH_TEMPERATURE))
+                .withInput(createYearAsText(), Arrays.asList(new IntWritable(HIGH_AIR_TEMPERATURE), new IntWritable(LOW_AIR_TEMPERATURE)))
+                .withOutput(createYearAsText(), new IntWritable(HIGH_AIR_TEMPERATURE))
                 .runTest();
     }
 }
